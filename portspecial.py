a241c5eaf07bd610666cf5548aa108d697c8b672
ted <tanyuliang2@gmail.com>
2016-07-22 10:20:27 +0800

    Change port for SSH and remote desktop

--- portspecial.py
+++ portspecial.py
@@ -2,11 +2,16 @@ import os
 IPS="10.1.1."#IP Range
 NATIP="222.239.87.195"#Public IP
 for num in range(100,256):
-    for num2 in range(int(str(num)+"00"),int(str(num)+"99")):
-      num2=num2+20000
-      command3="iptables -t nat -A PREROUTING -d "+NATIP+" -p all --dport "+str(num2)+" -j DNAT --to-destination "+IPS+str(num)+":"+str(num2)
-      command4="iptables -A INPUT -p tcp --dport "+str(num2)+" --syn -m recent --name webpool --rcheck --seconds 60 --hitcount 10 -j DROP"
-      os.system(command3)
+    for num1 in range(int(str(num)+"00"),int(str(num)+"99")):
+      num2=num1+20000
+      if str(num1) == str(num)+"00":
+        command1="iptables -t nat -A PREROUTING -d "+NATIP+" -p all --dport "+str(num2)+" -j DNAT --to-destination "+IPS+str(num)+":22"
+      elif  str(num1) == str(num)+"01":
+        command1="iptables -t nat -A PREROUTING -d "+NATIP+" -p all --dport "+str(num2)+" -j DNAT --to-destination "+IPS+str(num)+":3389"
+      else:      
+        command1="iptables -t nat -A PREROUTING -d "+NATIP+" -p all --dport "+str(num2)+" -j DNAT --to-destination "+IPS+str(num)+":"+str(num2)
+      command4="iptables -A INPUT -p tcp --dport "+str(num1)+" --syn -m recent --name webpool --rcheck --seconds 60 --hitcount 10 -j DROP"
+      os.system(command1)
       os.system(command4)
-      print command3
+      print command1
       print command4
